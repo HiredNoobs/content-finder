9f90a8926a86879461d3c662272e5251c874bc55
suicidejunkie <78608593+suicidejunkie@users.noreply.github.com>
2022-11-03 11:02:33 +0000

    Improve shorts blocking

--- cytubebot/contentfinder/content_finder.py
+++ cytubebot/contentfinder/content_finder.py
@@ -35,17 +35,13 @@ class ContentFinder:
             self._logger.info(f'Getting content for: {name}')
 
             channel = (
-                'https://www.youtube.com/feeds/videos.xml?channel_id=' f'{channel_id}'
+                f'https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}'
             )
             resp = requests.get(channel, timeout=60)
             page = resp.text
             soup = bs(page, 'lxml')
 
             for item in soup.find_all('entry'):
-                if '#shorts' in item.find_all('title')[0].text.casefold():
-                    self._logger.info('Skipping #short.')
-                    continue
-
                 published = item.find_all('published')[0].text
                 published = datetime.fromisoformat(published)
 
@@ -53,11 +49,33 @@ class ContentFinder:
                     self._logger.info(f'No more new videos for {name}')
                     break
 
+                title = item.find_all('title')[0].text.casefold()
                 video_id = item.find_all('yt:videoid')[0].text
 
-                c = ContentDetails(channel_id, published, video_id)
-                content.append(c)
+                if not self._is_short(title, video_id):
+                    c = ContentDetails(channel_id, published, video_id)
+                    content.append(c)
 
         content = sorted(content, key=attrgetter('datetime'))
 
         return content, len(content)
+
+    def _is_short(self, title: str, id: str) -> bool:
+        """
+        Returns True if video id is a YT Shorts video.
+        """
+        if '#shorts' in title:
+            return True
+
+        shorts_url = f'https://www.youtube.com/shorts/{id}'
+        resp = requests.head(
+            shorts_url, cookies={'CONSENT': 'YES+1'}, timeout=60, allow_redirects=False
+        )
+        if resp.status_code == 303:
+            return False
+        # Assume any 2XX successfully reached a shorts page
+        elif 200 <= resp.status_code <= 299:
+            return True
+        else:
+            self._logger.info(f'Received {resp.status_code=} from {shorts_url}')
+            return True
